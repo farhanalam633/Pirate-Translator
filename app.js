f42374f8c73cfa7e486112e233714131a35071e6
F ∆ R H ∆ N__∆ L ∆ M <farhanalam.dev@gmail.com>
2021-12-30 12:54:39 +0530

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+ 
+//var serverURL = "https://api.funtranslations.com/translate/ebonics.json"
+
+var serverURL ="https://api.funtranslations.com/translate/minion.json?input="
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something went wrong with server! try again after sometime")
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+     })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+
